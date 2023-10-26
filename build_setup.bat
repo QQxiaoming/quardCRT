0d58522f7d68c9ff886f51f5bc3194be77eabc3d
xiaoming <2014500726@smail.xtu.edu.cn>
2023-10-26 21:00:01 +0800

    ci:update window ci script
    Signed-off-by: xiaoming <2014500726@smail.xtu.edu.cn>

--- build_setup.bat
+++ build_setup.bat
@@ -38,8 +38,6 @@ del /f /q /a .\sed*
 xcopy /y .\build_release\out\quardCRT.exe .\InnoSetup\build\
 :: 使用windeployqt拷贝依赖dll库到打包目录
 windeployqt --dir .\InnoSetup\build .\InnoSetup\build\quardCRT.exe
-xcopy /y "%QT_TOOLS_DIR%\..\bin\lib*.dll" ".\InnoSetup\build\"
-xcopy /y "%QT_TOOLS_DIR%\..\lib\lib*.dll" ".\InnoSetup\build\"
 xcopy /y "D:\winpty\bin\winpty.dll" ".\InnoSetup\build\"
 xcopy /y "D:\winpty\bin\winpty-agent.exe" ".\InnoSetup\build\"
 xcopy /y .\scripts\Profile.ps1 .\InnoSetup\build\
