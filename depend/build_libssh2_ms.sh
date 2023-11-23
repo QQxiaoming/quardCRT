ea5671823cfffc6eedb97cabfd460930dcbf7c96
xiaoming <2014500726@smail.xtu.edu.cn>
2023-11-23 10:58:49 +0800

    ci:add msvc ci support
    Signed-off-by: xiaoming <2014500726@smail.xtu.edu.cn>

--- depend/build_libssh2_ms.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
-tar -xzvf libssh2-1.11.0.tar.gz
-mkdir output
-cd $SHELL_FOLDER/libssh2-1.11.0
-cmake -DCRYPTO_BACKEND=WinCNG -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=../output --build .
-cmake --build . --target install
