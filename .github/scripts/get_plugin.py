f2e33369b718ecdd08ec9fa6155458322364c603
xiaoming <2014500726@smail.xtu.edu.cn>
2024-02-20 13:40:29 +0800

    Add prebuilt plugins for msvc workfow
    Signed-off-by: xiaoming <2014500726@smail.xtu.edu.cn>

--- .github/scripts/get_plugin.py
+++ .github/scripts/get_plugin.py
@@ -4,9 +4,12 @@ import csv
 import platform
 import subprocess
 
-def get_file_name(fname, withmachine):
+def get_file_name(fname, withmachine, cc):
     os = platform.system()
     if os == 'Windows':
+        if cc == 'msvc':
+            if(withmachine):
+                return fname+'.msvc.dll'
         return fname+'.dll'
     elif os == 'Linux':
         return 'lib'+fname+'.so'
@@ -19,20 +22,24 @@ def get_file_name(fname, withmachine):
         return 'unknown'
 
 if __name__ == '__main__':
-    if len(sys.argv) != 3:
-        print('Usage: python get_plugin.py <csv file> <plugin dir>')
+    if len(sys.argv) < 3:
+        print('Usage: python get_plugin.py <csv file> <plugin dir> [cc]')
         exit(1)
     plugin_list_file = sys.argv[1]
     plugin_dir = sys.argv[2]
+    if len(sys.argv) == 4:
+        cc = sys.argv[3]
+    else:
+        cc = 'default'
     with open(plugin_list_file, 'r', encoding='utf-8') as f:
         reader = csv.reader(f)
         plugin_list = list(reader)
     for plugin_info in plugin_list:
         plugin_name = plugin_info[0]
-        plugin_url = plugin_info[1] + '/' +get_file_name(plugin_info[2], True)
+        plugin_url = plugin_info[1] + '/' +get_file_name(plugin_info[2], True, cc)
         print('Downloading plugin: ' + plugin_name)
         print('URL: ' + plugin_url)
-        plugin_path = os.path.join(plugin_dir, get_file_name(plugin_info[2],False))
+        plugin_path = os.path.join(plugin_dir, get_file_name(plugin_info[2],False, cc))
         subprocess.run(['curl', '-L', plugin_url, '-o', plugin_path])
         if platform.system() == 'Linux':
             subprocess.run(['chmod', '+x', plugin_path])
